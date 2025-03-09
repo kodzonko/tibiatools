29819db152de7758c40e751758289b49f0419808
Jan Wadolowski <janwadolowski@gmail.com>
2025-03-09 14:43:23 +0100

    Refactor project structure: remove main.py, add exercise_weapon_burnout_time command, and update dependencies

---
+++ tibiatools/main.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+import click
+
+
+@click.command()
+@click.argument("charges", type=click.IntRange(1))
+def exercise_weapon_burnout_time(charges: int):
+    """Given the number of charges of an exercise weapon, calculate the time it will burn out."""
+    current_time = datetime.now()
+    time_delta = charges * 2
+    future_time = current_time + timedelta(seconds=time_delta)
+
+    click.echo(
+        f"Exercise weapon will burn out at {future_time.strftime('%Y-%m-%d %H:%M:%S')}"
+    )
